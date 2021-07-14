90405da0552e3f786d7cbe829c549de4516420d1
Abhishek Negi <abhishek.negi@puresoft.int>
2021-07-14 14:04:35 +0530

    simple random number app

--- src/main.py
+++ src/main.py
@@ -1,39 +1,6 @@
-import folium
-import pandas
+# Program to generate a random number between 0 and 9
 
-data = pandas.read_csv("Volcanoes.txt")
-lat = list(data["LAT"])
-lon = list(data["LON"])
-elev = list(data["ELEV"])
+# importing the random module
+import random
 
-html = """<h4>Volcano information:</h4>
-Height: %s m
-"""
-
-def color_code(elevation):
-    if elevation < 1000:
-        return "green"
-    elif 1000 <= elevation < 3000:
-        return "orange"
-    else:
-        return "red"
-
-
-# map = folium.map
-map = folium.Map(location=[30.33990, 78.05362], zoom_start=6, tiles="Stamen Terrain")
-
-fgv = folium.FeatureGroup(name="Volcanoes")
-
-for lt, ln, el in zip(lat, lon, elev):
-    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fgv.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_code(el))))
-
-
-fgp = folium.FeatureGroup(name="Population")
-fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='UTF-8-sig').read(),
-                            style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
-                            else 'orange' if 10000000 <= x['properties'] ['POP2005'] < 20000000 else 'red'}))
-map.add_child(fgv)
-map.add_child(fgp)
-map.add_child(folium.LayerControl())
-map.save("Map1.html")
+print(random.randint(0,9))
